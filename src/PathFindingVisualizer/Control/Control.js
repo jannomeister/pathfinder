295e5a4bcd7137e32b4b1784f05a291a14ab4e00
Janno Tabamo <janno@hyperstacksinc.com>
2020-08-11 10:16:51 +0800

    feat: improved UI

--- src/PathFindingVisualizer/Control/Control.js
+++ src/PathFindingVisualizer/Control/Control.js
@@ -1,23 +1,22 @@
 import React, { useState } from 'react';
+import Legend from './Legend/Legend';
 import DiagonalOption from './DiagonalOption/DiagonalOption';
 import Heuristic from './Heuristic/Heuristic';
 
-// import FormControl from '@material-ui/core/FormControl';
-// import InputLabel from '@material-ui/core/InputLabel';
-// import Select from '@material-ui/core/Select';
-// import MenuItem from '@material-ui/core/MenuItem';
-import Grid from '@material-ui/core/Grid';
-import Alert from '@material-ui/lab/Alert';
-// import Button from '@material-ui/core/Button';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import Container from 'react-bootstrap/Container';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 
 import './Control.css';
 
-const Control = ({ start, finish, hasMaze, visualize, generateMaze, reset }) => {
+const Control = ({ start, finish, hasMaze, visualize, generateMaze, clearPath, reset }) => {
   const [algorithm, setAlgorithm] = useState("");
   const [heuristic, setHeuristic] = useState("manhattan");
   const [isDiagonal, setIsDiagonal] = useState(false);
   const canVisualize = !start || !finish ? true : false;
-  const canGenerate = !start || !finish ? true : false;
+  const canGenerate = hasMaze || !start || !finish ? false : true;
 
   const onVisualize = e => {
     e.preventDefault();
@@ -27,38 +26,56 @@ const Control = ({ start, finish, hasMaze, visualize, generateMaze, reset }) => 
   }
 
   return (
-    <Grid item xs={12}>
-      <div>
-        { !algorithm && <Alert severity="warning">Please select an algorithm</Alert> }
-        <form>
-          <select value={algorithm} onChange={e => setAlgorithm(e.target.value)}>
-            <option value="">none</option>
+    <div style={{ padding: 10 }}>
+      <Legend />
+
+      <Form>
+        <ButtonGroup style={{ marginBottom: 10 }}>
+          <Button variant="outline-secondary" disabled={!canGenerate} onClick={generateMaze}>
+            Generate Maze
+          </Button>
+          <Button variant="outline-secondary" disabled={!hasMaze} onClick={clearPath}>
+            Clear Path
+          </Button>
+          <Button variant="outline-secondary"
+            onClick={() => {
+              setAlgorithm("");
+              setHeuristic("manhattan");
+              setIsDiagonal(false);
+              reset(); 
+            }}
+          >
+            Reset
+          </Button>
+        </ButtonGroup>
+
+        <Container style={{ marginBottom: 10 }}>
+          <FormControl
+            as="select"
+            size="sm"
+            value={algorithm}
+            onChange={e => setAlgorithm(e.target.value)}
+          >
+            <option value="">Select Algorithm</option>
             <option value="dijkstra">Dijkstra</option>
             <option value="astar">A star</option>
             <option value="BFS">Breadth First Search</option>
-          </select>
+          </FormControl>
+        </Container>
 
-          <button disabled={canVisualize} onClick={onVisualize}>Visualize</button>
+        {algorithm === "astar" && (
+          <Heuristic heuristic={heuristic} onChange={e => setHeuristic(e.target.value)} />
+        )}
 
-          {algorithm === "astar" && (
-            <DiagonalOption isDiagonal={isDiagonal} onChange={e => setIsDiagonal(e.target.checked)} />
-          )}
+        {algorithm === "astar" && (
+          <DiagonalOption isDiagonal={isDiagonal} onChange={e => setIsDiagonal(e.target.checked)} />
+        )}
 
-          {algorithm === "astar" && (
-            <Heuristic heuristic={heuristic} onChange={e => setHeuristic(e.target.value)} />
-          )}
-        </form>
-      </div>
-      <div>
-        <button disabled={canGenerate} onClick={generateMaze}>Generate Maze</button>
-        <button
-          onClick={() => {
-            setAlgorithm("");
-            reset();
-          }}
-        >Reset</button>
-      </div>
-    </Grid>
+        <Button variant="outline-success" disabled={canVisualize} onClick={onVisualize}>
+          Visualize
+        </Button>
+      </Form>
+    </div>
   )
 }
 
